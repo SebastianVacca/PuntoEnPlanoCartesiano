eeea9cc0fcf3841687815677535a2eca74386297
Juan Vacca <clanbs11.12@gmail.com>
2025-07-15 19:41:37 -0500

    Add use of methods

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var punto = new Punto();
+
+        System.out.println(punto.mostrarCoordenadas());
+
+        punto.mover(3.2, 5.6);
+
+        System.out.println(punto.mostrarCoordenadas());
+
+        System.out.println(punto.calcularDistancia());
+
     }
 }
